42cdfeb695418829c9389b506fd78663f80a43b2
mrkody <kody1994@mail.ru>
2016-11-15 21:15:55 +0300

    first

---
+++ src/Printbar.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Mrkody\Printbar;
+
+use Illuminate\Support\Facades\Log;
+
+
+/**
+ * Class Printbar
+ *
+ * @author mrkody kody1994@mail.ru
+ *
+ * @package Mrkody\Printbar
+ */
+
+class Printbar {
+    
+}
